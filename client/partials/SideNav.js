172310a8c0af2e2830d26009952cccbe7102a326
kellymersereau <kelly.mersereau@gmail.com>
2016-08-11 10:42:19 -0400

    fixing styling for header and navbar

--- client/partials/SideNav.js
+++ client/partials/SideNav.js
@@ -11,9 +11,36 @@ Template.SideNav.helpers({
 	},
 });
 Template.SideNav.events({
-    'click .logout': function(event){
+    'click #logout': function(event){
         event.preventDefault();
         Meteor.logout();
         FlowRouter.go('home');
+    },
+    'click a': function(event){
+    	$('div').each(function(){
+    		if($(this).hasClass("activestep")){
+    			$(this).removeClass("activestep");
+    		}
+    	});
+    	if($(this.parentNode).hasClass("col-md-2")){
+    		$(this.parentNode).addClass("activestep");
+    	} else{
+    		$(this.parentNode).addClass("activestep");
+    	}
     }
 });
+
+// function resetActive(event) {
+//     $("div").each(function () {
+//         if ($(this).hasClass("activestep")) {
+//             $(this).removeClass("activestep");
+//         }
+//     });
+
+//     if (event.target.className == "col-md-2") {
+//         $(event.target).addClass("activestep");
+//     }
+//     else {
+//         $(event.target.parentNode).addClass("activestep");
+//     }
+// }
